ddaa8a648ea7bb3240ea42af6241d2f317d85e2e
B.Yevhen <y.bulatovych@gmail.com>
2020-02-02 19:05:54 +0200

    account servlet

--- src/main/java/ua/epam/crud/rest/AccountServlet.java
+++ src/main/java/ua/epam/crud/rest/AccountServlet.java
@@ -47,17 +47,66 @@ public class AccountServlet extends HttpServlet {
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        try {
+            PrintWriter writer = resp.getWriter();
+            resp.setContentType("application/json");
+            resp.setCharacterEncoding("UTF-8");
+            Long id = Long.parseLong(req.getParameter("id"));
+            Long id_status = Long.parseLong(req.getParameter("id_status"));
+            accountService.create(accountService.createAccount(id, id_status));
+            Account accountById = accountService.getById(Long.parseLong(req.getParameter("id")));
+            writer.println(gson.toJson(accountById));
+            writer.flush();
+
+        } catch (IOException e) {
+            PrintWriter writer = resp.getWriter();
+            resp.setContentType("application/json");
+            resp.setCharacterEncoding("UTF-8");
+            resp.sendError(400);
+            writer.flush();
+        }
     }
 
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPut(req, resp);
+        try {
+            PrintWriter writer = resp.getWriter();
+            resp.setContentType("application/json");
+            resp.setCharacterEncoding("UTF-8");
+            Long id = Long.parseLong(req.getParameter("id"));
+            Long id_status = Long.parseLong(req.getParameter("id_status"));
+            accountService.update(accountService.createAccount(id, id_status));
+            Account accountById = accountService.getById(Long.parseLong(req.getParameter("id")));
+            writer.println(gson.toJson(accountById));
+            writer.flush();
+
+        } catch (IOException e) {
+            PrintWriter writer = resp.getWriter();
+            resp.setContentType("application/json");
+            resp.setCharacterEncoding("UTF-8");
+            resp.sendError(400);
+            writer.flush();
+        }
     }
 
     @Override
-    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doDelete(req, resp);
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        try {
+            PrintWriter writer = resp.getWriter();
+            resp.setContentType("application/json");
+            resp.setCharacterEncoding("UTF-8");
+            Long id = Long.parseLong(req.getParameter("id"));
+            accountService.delete(id);
+            writer.println(gson.toJson(accountService.getAll()));
+            writer.flush();
+
+        } catch (IOException e) {
+            PrintWriter writer = resp.getWriter();
+            resp.setContentType("application/json");
+            resp.setCharacterEncoding("UTF-8");
+            resp.sendError(400);
+            writer.flush();
+        }
     }
 }
